253f1f3777edf7589de25d9bb839e02f93d2294a
Evelyn dos Santos <evelynsantos@ibm.com>
2022-08-01 15:48:46 -0300

    :sparkles: Implementando metodo salvar artigos  :sparkles:

--- database/database.js
+++ database/database.js
@@ -3,7 +3,8 @@ const Sequelize = require("sequelize");
 // Conexão
 const connection = new Sequelize('guiapress', 'root', 'Borys@02',{
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00"
 });
 
 module.exports = connection;
