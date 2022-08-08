1bbc12ca45e66da5391d8de5e995b0c33c4b2d62
Evelyn dos Santos <evelynsantos@ibm.com>
2022-08-08 13:37:12 -0300

    :sparkles: Rota de paginação de artigos  :sparkles:

--- articles/ArticlesControlles.js
+++ articles/ArticlesControlles.js
@@ -33,7 +33,7 @@ router.post("/articles/save", (req, res) => {
     })
 });
 
-router.post("/articles/delete", (req,res)=> {
+router.post("/articles/delete", (req,res) => {
     var id = req.body.id;
     if(id != undefined){
         if(!isNaN(id)){
@@ -55,5 +55,75 @@ router.post("/articles/delete", (req,res)=> {
         res.redirect("/admin/articles");
     }
 });
+
+
+router.get("/admin/articles/edit/:id", (req,res) => {
+    var id = req.params.id;
+    Article.findByPk(id).then(article => {
+        if(article != undefined){
+           Category.findAll().then(categories => {
+               res.render("admin/articles/edit", {article: article, categories: categories});
+           });
+        }
+        else{
+            res.redirect("/admin/articles");
+        }
+    }).catch( err => {
+        res.redirect("/admin/articles");
+    });
+});
+
+router.post("/articles/update", (req, res) => {
+    var id = req.body.id;
+    var title = req.body.title;
+    var body = req.body.body;
+    var category = req.body.category
+
+    Article.update({title: title, body: body, categoryId: category, slug:slugify(title)},{
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles");
+    }).catch(err => {
+        res.redirect("/");
+    });
+});
+
+router.get("/articles/page/:num", (req, res) => {
+    var page = req.params.num;
+    var offset = 0;
+
+    if(isNaN(page) || page == 1){
+        offset = 0;
+    }
+    else{        
+        var offset = parseInt(page);
+    }
+
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset 
+    }).then(articles=>{
+
+        var next;
+        if(offset +4 > articles.count){
+            next = false;
+        }
+        else{
+            next = true;
+        }
+
+        var result = { 
+            next: next,
+            articles: articles
+        }
+
+        Category.findAll().then( categories => {
+            res.render("admin/articles/page", { result: result, categories: categories});
+        });
+
+    })
+})
 
 module.exports = router;
